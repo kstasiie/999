2c4bf13b9d3e222306a65ec81124a70a6d9ea268
kstasiie <amkopylova@edu.hse.ru>
2025-02-04 00:22:42 +0500

    RunnerArray is done!

--- 999/Program.cs
+++ 999/Program.cs
@@ -1,7 +1,25 @@
-﻿namespace _999
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.Testing.Platform.Extensions.Messages;
+
+namespace _999
 {
     internal class Program
     {
+        public static void SortRunners(RunnerArray runnerArray) 
+        {
+            for (int i = 0; i < runnerArray.Length - 1; i++)
+            {
+                for (int j = 0; j < runnerArray.Length - i - 1; j++) 
+                {
+                    if (runnerArray[j].Distance < runnerArray[j + 1].Distance || runnerArray[j].Distance == runnerArray[j + 1].Distance && runnerArray[j].CalculatingTime() > runnerArray[j + 1].CalculatingTime())
+                    {
+                        Runner tempRunner = runnerArray[j];
+                        runnerArray[j] = runnerArray[j + 1];
+                        runnerArray[j + 1] = tempRunner;
+                    }
+                }
+            }
+        }
         static void Main(string[] args)
         {
             try
@@ -9,48 +27,51 @@
                 //создание
                 Console.WriteLine("Создание");
 
+                Console.WriteLine("\nКонструктор без параметров\n");
+                Runner runner = new Runner();
+                runner.Show();
+
+                Console.WriteLine("\nКонструктор с параметрами\n");
                 Runner runner1 = new Runner(15, 10);
                 runner1.Show();
                 Runner runner2 = new Runner(15, 100);
                 runner2.Show();
 
                 //вычисление времени статическим и нестатическим методом
-                Console.WriteLine("\nВычисление времени статическим и нестатическим методом\n");
-
-                Console.WriteLine("Вычисление времени нестатическим методом");
+                
+                Console.WriteLine("\nВычисление времени нестатическим методом\n");
                 double time2 = runner2.CalculatingTime();//нестатический метод
                 Console.WriteLine(time2);
 
-                Console.WriteLine("\nВычисление времени статическим методом");
+                Console.WriteLine("\nВычисление времени статическим методом\n");
                 double time3 = Runner.CalculatingTime(50, 100);//статический метод
                 Console.WriteLine(time3);
 
 
                 //количество созданных объектов
-                Console.WriteLine("\nКоличество созданных объектов");
+                Console.WriteLine("\nКоличество созданных объектов\n");
 
                 Console.WriteLine(Runner.GetCount);
 
                 //перегрузка
-                Console.WriteLine("\nПерегрузка\n");
 
                 //унарные операции
 
-                Console.WriteLine("Перегрузка операции ++ (увеличивает расстояние на 0.1 км)");
+                Console.WriteLine("\n\n\nПерегрузка операции ++ (увеличивает расстояние на 0.1 км)\n");
                 runner1++;
                 runner1.Show();
 
-                Console.WriteLine("\nПерегрузка операции -- (уменьшает скорость на 0.05 км/ч)");
+                Console.WriteLine("\nПерегрузка операции -- (уменьшает скорость на 0.05 км/ч)\n");
                 runner2--;
                 runner2.Show();
 
                 //бинарные операции
 
-                Console.WriteLine("\nПерегрузка операции - (вычисляет расстояния, на котором встретятся два бегун)");
+                Console.WriteLine("\nПерегрузка операции - (вычисляет расстояния, на котором встретятся два бегун)\n");
                 double mt = runner1 - runner2;//Вычисление расстояния, на котором встретятся два бегуна
                 Console.WriteLine(mt);
 
-                Console.WriteLine("\nПерегрузка операции ^ (увеличивает скорость на какую - то величину)");
+                Console.WriteLine("\nПерегрузка операции ^ (увеличивает скорость на какую-то величину)\n");
                 runner1 = runner1 ^ 4;//Увеличение скорости на величину
                 runner1.Show();
 
@@ -59,18 +80,54 @@
 
                 //операции приведение типа
 
-                Console.WriteLine("\nЯвное приведение к double (возвращает значение, на которое нужно увеличить скорость)");
+                Console.WriteLine("\n\n\nЯвное приведение к double (возвращает значение, на которое нужно увеличить скорость)\n");
                 double increasedSpeed = (double)runner1;//явное приведение к double
                 Console.WriteLine(increasedSpeed);
 
-                Console.WriteLine("\nНеявное приведение к string (возвращает время, за которое преодалел дистанцию)");
+                Console.WriteLine("\nНеявное приведение к string (возвращает время, за которое преодалел дистанцию)\n");
                 string timeString = runner1;//неявное приведение к string
                 Console.WriteLine(timeString);
 
-                Console.WriteLine("\nЧасть 3");
+                Console.WriteLine("\n\n\nКласс-коллекция");
+
+                Console.WriteLine("\nКонструктор без параметров\n");
+                RunnerArray ra = new RunnerArray();
+                ra.Show();
+
+                Console.WriteLine("\nКонструктор с параметром\n");
                 RunnerArray ra1 = new RunnerArray(5);
                 ra1.Show();
 
+                Console.WriteLine("\nКонструктор копирования\n");
+                RunnerArray ra2 = new RunnerArray(ra1);
+                ra2.Show();
+
+                ra1[0].Speed = 100;
+                Console.WriteLine("\nИзмененный массив");
+                ra1.Show();
+                Console.WriteLine("\nСкопированный массив (до изменения)\n");
+                ra2.Show();
+
+                Console.WriteLine("\nЗапись объекта и его получение с существующим индексом\n");
+                RunnerArray ra3 = new RunnerArray(5);
+                Console.WriteLine("Изначальный массив\n");
+                ra3.Show();
+                Console.WriteLine("\nЗапись в элемент с индексом 3 значения скорости 100 и его вывод\n");
+                ra3[3].Speed = 100;
+                ra3[3].Show();
+                //Console.WriteLine("\nЗапись в элемент с индексом 10 значения скорости 100 и его вывод\n");
+                //ra3[10].Speed = 100;
+                //ra3[10].Show();
+
+                Console.WriteLine("\nСортировка массива бегунов");
+                Console.WriteLine("\nДо сортировки\n");
+                ra1.Show();
+                Console.WriteLine("\nПосле сортировки\n");
+                SortRunners(ra1);
+                ra1.Show();
+
+                Console.WriteLine("\nКоличество созданных коллекций\n");
+                Console.WriteLine(RunnerArray.GetCount);
 
             }
             catch (Exception ex)
@@ -79,7 +136,7 @@
             }
             finally
             {
-                Console.WriteLine("Вычисления окончены!");
+                Console.WriteLine("\nВычисления окончены!");
             }
         }
     }
--- 999/RunnerArray.cs
+++ 999/RunnerArray.cs
@@ -10,6 +10,13 @@ namespace _999
     {
         static Random random = new Random();
 
+        static int count;
+
+        public static int GetCount
+        {
+            get => count; 
+        }
+
         Runner[] runnerArray;
 
         public int Length => runnerArray.Length;
@@ -17,15 +24,25 @@ namespace _999
         public RunnerArray()
         {
             runnerArray = new Runner[0];
+            count++;
         }
 
         public RunnerArray(int length)
         {
             runnerArray = new Runner[length];
+            count++;
             for (int i = 0; i < length; i++)
             {
                 runnerArray[i] = new Runner(random.Next(1, 100), random.Next(1, 1000));
             }
+        }
+
+        public RunnerArray(RunnerArray other)
+        {
+            runnerArray = new Runner[other.Length];
+            count++;
+            for (int i = 0; i < other.Length; i++)
+                runnerArray[i] = new Runner (other[i].Speed, other[i].Distance);
         }
 
         public void Show()
@@ -33,6 +50,22 @@ namespace _999
             for (int i = 0; i < runnerArray.Length; i++)
             {
                 runnerArray[i].Show();
+            }
+        }
+
+        public Runner this[int index]
+        {
+            get 
+            { 
+                if (index >= 0 && index < runnerArray.Length)
+                return runnerArray[index];
+                else throw new Exception("Выход за границы массива");
+            }
+            set 
+            {
+                if (index >= 0 && index < runnerArray.Length)
+                    runnerArray[index] = value;
+                else throw new Exception("Выход за границы массива");
             }
         }
     }
